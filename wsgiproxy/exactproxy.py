41eebcca8e0874dc59bc0962a521777f4a8eaf84
Nielson Santana <nielsonnas@gmail.com>
2016-07-12 01:43:19 -0300

    Hacking on proxy_exact_request to enable change headers_out after

--- wsgiproxy/exactproxy.py
+++ wsgiproxy/exactproxy.py
@@ -11,6 +11,46 @@ filtered_headers = (
     'transfer-encoding',
 )
 
+class L(list):
+    """
+    A subclass of list that can accept additional attributes.
+    Should be able to be used just like a regular list.
+
+    The problem:
+    a = [1, 2, 4, 8]
+    a.x = "Hey!" # AttributeError: 'list' object has no attribute 'x'
+
+    The solution:
+    a = L(1, 2, 4, 8)
+    a.x = "Hey!"
+    print a       # [1, 2, 4, 8]
+    print a.x     # "Hey!"
+    print len(a)  # 4
+
+    You can also do these:
+    a = L( 1, 2, 4, 8 , x="Hey!" )                 # [1, 2, 4, 8]
+    a = L( 1, 2, 4, 8 )( x="Hey!" )                # [1, 2, 4, 8]
+    a = L( [1, 2, 4, 8] , x="Hey!" )               # [1, 2, 4, 8]
+    a = L( {1, 2, 4, 8} , x="Hey!" )               # [1, 2, 4, 8]
+    a = L( [2 ** b for b in range(4)] , x="Hey!" ) # [1, 2, 4, 8]
+    a = L( (2 ** b for b in range(4)) , x="Hey!" ) # [1, 2, 4, 8]
+    a = L( 2 ** b for b in range(4) )( x="Hey!" )  # [1, 2, 4, 8]
+    a = L( 2 )                                     # [2]
+    """
+    def __new__(self, *args, **kwargs):
+        return super(L, self).__new__(self, args, kwargs)
+
+    def __init__(self, *args, **kwargs):
+        if len(args) == 1 and hasattr(args[0], '__iter__'):
+            list.__init__(self, args[0])
+        else:
+            list.__init__(self, args)
+        self.__dict__.update(kwargs)
+
+    def __call__(self, **kwargs):
+        self.__dict__.update(kwargs)
+        return self
+
 def filter_paste_httpserver_proxy(app):
     """
     Maps the ``paste.httpserver`` proxy environment keys to
@@ -105,7 +145,10 @@ def proxy_exact_request(environ, start_response):
     else:
         body = res.read()
     conn.close()
-    return [body]
+    # Hacking to change headers_out after
+    response = L(body)
+    response.dict_response = {'status':status, 'headers_out': headers_out}
+    return response
 
 def parse_headers(message):
     """
